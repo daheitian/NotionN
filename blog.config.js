aa0a44197580af045c98b06306beb35e15302321
jijiang hou <113097833+daheitian@users.noreply.github.com>
2025-11-18 13:31:11 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
    AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '小信号', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '设计  思考  写作', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || ''https://xiaoxinhao.top', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://xiaoxinhao.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
